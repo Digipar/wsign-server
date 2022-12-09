34ba6199fd136ec48c57474b03463e83818f4962
Victor Martinetti <victor.martinetti@digipar.com>
2022-12-09 13:53:22 -0300

    Get PORT from process.env

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+const PORT = process.env.PORT || 3000; 
 const app = require('./app');
 // open webserver port 3000
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
+app.listen(PORT, () => {
+    console.log('Server started on port '+PORT);
 });
