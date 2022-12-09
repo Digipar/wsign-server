a4750374ecc61791b499ffb3a086ee078e868880
Victor Martinetti <victor.martinetti@digipar.com>
2022-12-09 10:54:51 -0300

    no XML received treated

--- routes/facturas.js
+++ routes/facturas.js
@@ -6,6 +6,10 @@ const facturasController = require('../controllers/facturas');
 /* POST to sign an xml, with validation middleware. */
 router.post('/firmar',validateInvoice, function(req, res, next) {
   // console.log('  req.body',   Buffer.from(req.body).toString('utf8'));
+  if(req.body === undefined) {
+    res.status(400).send('No XML received');
+    return;
+  }
   const xmlReceived = Buffer.from(req.body).toString('utf8');
   const xmlSigned = facturasController.signXML(xmlReceived);  
   res.send(xmlSigned);
